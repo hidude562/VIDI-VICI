9a0461f45767b7035df1fefa98cfc6c02406cba9
Nathan F <79991107+Pyro569@users.noreply.github.com>
2024-10-09 08:59:59 -0400

    Time Push

--- game/scripts/globals.gd
+++ game/scripts/globals.gd
@@ -1,38 +1,21 @@
 extends Node
 
-var year = 1750
+var year = 1752
 var month = 1
 var day = 1
+
+var paused = true
+var timer = Timer.new()
+
+var months = ["Random obligatory empty/pointless string Because I don't want to always do month - 1", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+var daysInMonths = [300, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 func getYear():
 	return str(year) + " AD"
 
 func getMonth():
-	match month:
-		1:
-			return "January"
-		2:
-			return "February"
-		3:
-			return "March"
-		4:
-			return "April"
-		5:
-			return "May"
-		6:
-			return "June"
-		7:
-			return "July"
-		8:
-			return "August"
-		9:
-			return "September"
-		10:
-			return "October"
-		11:
-			return "November"
-		12:
-			return "December"
+	return months[month]
+	
 func getDate():
 	return str(day)
 	
@@ -42,7 +25,42 @@ func getFullYear():
 func _ready() -> void:
 	var dateDisplay = get_node("/root/Game/Control/Top display/ColorRect/Label")
 	dateDisplay.text = getFullYear()
-
+	add_child(timer)
+	timer.wait_time = 0.2
+	timer.timeout.connect(_on_timer_timeout)
+	
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("time"):
+		if paused:
+			paused = false
+			timer.start(0)
+			print("Unpaused")
+		else:
+			paused = true
+			timer.stop()
+			print("Paused")
+			
+func _process(delta: float) -> void:
+	var dateDisplay = get_node("/root/Game/Control/Top display/ColorRect/Label")
+	dateDisplay.text = getFullYear()
+	
+func _on_timer_timeout():
+	#leap years
+	if getMonth() == "February":
+		if day == 28:
+			if year % 4 == 0 and year % 400 == 0:
+				day += 1
+		elif day < daysInMonths[month]:
+			day += 1
+		else:
+			month += 1
+			day = 1
+	elif day < daysInMonths[month]:
+		day += 1
+	else:
+		month += 1
+		day = 1
+	
 class Nation:
 	var provinces
 	var name
